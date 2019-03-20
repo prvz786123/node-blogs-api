cdedf170c59eef49e0181b770c3677a551af85d2
apple <parvez.s@canishub.com>
2019-03-20 11:05:02 +0530

    Cors Setup

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const bodyParser = require('body-parser');
 const blogs_route = require('./routes/blogs_route')
+const cors = require('cors')
+
 
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json());
+
+app.use(cors());
 
 app.use("/blogs",blogs_route)
 
